2a230b200d0ca282456cbc3b47214811a248e0f4
CodingwithJess <thejessiebess@gmail.com>
2020-07-07 23:18:36 -0600

    styling added for input, search, and container

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,45 @@
+// global variables
+var APIKey = "748841953b1647f23e37f49111c4e30a";
+var lsKey = "weatherSearches"
+var searchesDiv = $("#searches");
+var searchInput = $("#searchInput");
+var searchButton = $("#searchBtn");
+var currentWeatherDiv = $("#currentWeather");
+var forecastDiv = $("#forecast");
+var clearBtn = $("#clear");
+var storedSearches = getStoredSearches();
+//variable used to store and determine if the city needs to be added to the search history
+var addedCity = newCity();
+//unit variables for future development of switching between unit systems.
+var metricUnits = {deg:"C", speed:"KPH"};
+var impUnits = {deg:"F",speed:"MPH"};
+var units = metricUnits;
 
+
+
+
+
+function searchButtonClicked(){  
+  var cityVal = searchInput.val().trim();
+  var city = newCity(cityVal, null);       
+  getWeather(city);
+  //clear the value once the search is activated
+  searchInput.val("");        
+}
+
+function getWeather(city){
+  addedCity = city; 
+  let queryURLCurrent = "";
+  let queryURLForecast = "";
+
+  if(city.country == null){
+      queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+city.city+"&units=metric&appid="+APIKey;
+      queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+city.city+"&units=metric&appid="+APIKey;
+  }else{        
+      queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+city.city+","+city.country+"&units=metric&appid="+APIKey;
+      queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+city.city+","+city.country+"&units=metric&appid="+APIKey;
+  }
+  
+  performAPIGETCall(queryURLCurrent, buildCurrentWeather);
+  performAPIGETCall(queryURLForecast, buildForecastWeather);    
+}
